26d47f5ea46fd1ea7bb0385ed945b0146734ae7c
mediaBrilliance <dez@mediabrilliance.io>
2026-02-07 15:10:03 -0600

    Initial Commit

---
+++ polaGram/polaGramApp.swift
@@ -0,0 +1,17 @@
+//
+//  polaGramApp.swift
+//  polaGram
+//
+//  Created by valdez campos on 2/7/26.
+//
+
+import SwiftUI
+
+@main
+struct polaGramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
